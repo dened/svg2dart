f95874122698d5dda2a157910f7219999ed960bf
Denis Chuvasov <chuvasov.d@gmail.com>
2025-09-24 09:07:35 +0200

    test: add initial test structure with example test case

--- test/svg2dart_test.dart
+++ test/svg2dart_test.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  group('Example test', () {
+    setUp(() {
+    });
+    test('First Test', () {
+      expect(true, isTrue);
+    });
+  });
+  
+}
